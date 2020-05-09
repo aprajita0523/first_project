52bb5fcb42daa54c8cb49a91109e9121c1bac659
aprajita0523 <aprajitaaditya@gmail.com>
2020-05-09 20:04:00 +0530

    third commit

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,AccessRecord,Webpage
 '''
 print('hello')
 def index(request):
@@ -8,7 +9,10 @@ def index(request):
 
 
 def index(request):
-    return HttpResponse("<em>hello First Project</em>")
+    #return HttpResponse("<em>hello First Project</em>")
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpage_list}
+    return render(request,"first_app1/index.html",context=date_dict)
 
 print('hi')
 def help(request):
